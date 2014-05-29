5999e277e89dd3cecb39df227409fe27d00290db
Raghu Betina <git@rugb.com>
2014-05-29 14:23:03 -0500

    Scaffolded Comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
+
   root 'photos#index'
 
-  # CREATE
-  get    'photos/new' => 'photos#new', :as => 'new_photo'
-  post   'photos' => 'photos#create', :as => 'photos'
-
-  # READ
-  get    'photos' => 'photos#index'
-  get    'photos/:id' => 'photos#show', :as => 'photo'
-
-  # UPDATE
-  get    'photos/:id/edit' => 'photos#edit', :as => 'edit_photo'
-  patch  'photos/:id' => 'photos#update'
-
-  # DELETE
-  delete 'photos/:id' => 'photos#destroy'
+  resources :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528192425) do
+ActiveRecord::Schema.define(version: 20140529191352) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "photo_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "caption"
