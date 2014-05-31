2dde20c04bfb3c6644849c7d1386f8ae74a723a9
Raghu Betina <git@rugb.com>
2014-05-30 19:04:59 -0500

    some stuff

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,8 @@ class Photo < ActiveRecord::Base
 
   has_many :comments, :dependent => :destroy
   belongs_to :user
+  has_many :favorites
+  has_many :fans, :through => :favorites, :source => :user
 
   mount_uploader :image, ImageUploader
 
