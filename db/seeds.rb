dc7da180f1744d1844f3e4a2dfa1bdb383340bad
Raghu Betina <git@rugb.com>
2014-06-02 09:18:05 -0500

    Fixed seeds to work with file uploads

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,8 +41,6 @@
 
 # puts "There are now #{Comment.count} comments."
 
-require 'open-uri'
-
 users = [
   { :email => "raghu@example.com",
     :password => "11111111",
@@ -63,19 +61,16 @@ User.create users
 puts "There are now #{User.count} users."
 
 photo_hashes = [
-  { :caption => "Peyton Manning", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/3/30/Peyton_Manning_%28cropped%29.jpg") },
-  { :caption => "Drew Brees", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/9/98/Drew_Brees_at_Saints_Super_Bowl_parade_2010-02-09.jpg") },
-  { :caption => "Aaron Rodgers", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/1/1c/Aaron_Rodgers_2008_%28cropped%29.jpg") },
-  { :caption => "Cam Newton", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/9/95/Cam_Newton_-_Carolina_Panthers.jpg") },
-  { :caption => "Matt Stafford", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/6/60/Matt_Stafford_2009_cropped.jpg") },
-  { :caption => "Andrew Luck", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/d/d9/Andrew_Luck_at_2010_Stanford_football_open_house.JPG") },
-  { :caption => "Russell Wilson", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/f/f3/Russell_Wilson_at_the_2013_Jessie_Vetter_Classic%2C_July_1%2C_2013.jpg") },
-  { :caption => "Colin Kaepernick", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/1/1b/Colin_Kaepernick_-_San_Francisco_vs_Green_Bay_2012.jpg") },
-  { :caption => "Tom Brady", :image => File.open("http://upload.wikimedia.org/wikipedia/commons/9/9f/Tom_Brady_2011.JPG") }
+  { :caption => "Peyton Manning", :image_url => "pm.jpg" },
+  { :caption => "Drew Brees", :image_url => "db.jpg" },
+  { :caption => "Aaron Rodgers", :image_url => "ar.jpg" },
+  { :caption => "Cam Newton", :image_url => "cm.jpg" }
 ]
 
 photo_hashes.each do |photo_hash|
-  photo = Photo.new photo_hash
+  photo = Photo.new
+  photo.caption = photo_hash[:caption]
+  photo.image = File.open("/Users/student/code/enova/instagram/lib/seed_images/#{photo_hash[:image_url]}")
 
   random_user = User.offset(rand(User.count)).limit(1).first
   photo.user = random_user
@@ -86,8 +81,8 @@ end
 puts "There are now #{Photo.count} photos."
 
 favorite_hashes = [
-  { :nickname => "Raghu", :captions => ["Peyton Manning", "Andrew Luck"] },
-  { :nickname => "Roneesh", :captions => ["Matt Stafford", "Tom Brady"] }
+  { :nickname => "Raghu", :captions => ["Peyton Manning", "Aaron Rodgers"] },
+  { :nickname => "Roneesh", :captions => ["Drew Brees", "Cam Newton"] }
 ]
 
 favorite_hashes.each do |favorite_hash|
