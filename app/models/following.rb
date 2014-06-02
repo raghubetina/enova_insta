c667677a0f24f632e22c8c568249bbff574bb211
Raghu Betina <git@rugb.com>
2014-06-02 10:56:46 -0500

    Added partial for photo, need partial for comment

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,7 @@
 class Following < ActiveRecord::Base
-  belongs_to :leader, :class_name => "User"
-  belongs_to :follower, :class_name => "User"
+  belongs_to :leader, :class_name => "User", :foreign_key => "leader_id"
+  belongs_to :follower, :class_name => "User", :foreign_key => "follower_id"
+
+  validates :follower, :uniqueness => { :scope => :leader, :message => "already follows this user" }, :presence => true
+  validates :leader, :presence => true
 end
